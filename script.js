9af0a95839b01728b2a4c4919677a0240b7ef252
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-09-30 20:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 let sortedBand=bands.sort((a,b)=>a.replace(/^(a |the |an )/i, "").localeCompare(b.replace(/^(a |an |the )/i,"")));
 
-let bandNames = document.getElementById("band");
+let bandNames = document.getElementById("bands");
 for(let i =0;i<sortedBand.length;i++)
 	{
 		
